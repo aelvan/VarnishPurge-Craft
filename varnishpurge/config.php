33c100d95ce4acb8b06173bb4de62d438faab34d
Tim Kelty <tim@fusionary.com>
2016-11-02 10:54:04 -0400

    purgeEnabled default

--- varnishpurge/config.php
+++ varnishpurge/config.php
@@ -3,7 +3,7 @@ namespace Craft;
 
 return array(
     'varnishUrl' => craft()->getSiteUrl(),
-    'purgeEnabled' => false,
+    'purgeEnabled' => isset($_SERVER['HTTP_X_VARNISH']),
     'purgeRelated' => true,
     'logAll' => 0,
     'purgeUrlMap' => [],
