6ac1bd7fbfa60455426636be7ec618924669ad2b
André Elvan <andre@vaersaagod.no>
2016-12-20 20:53:42 +0100

    Bumped version. Created releases log and refactored plugin information.

--- varnishpurge/VarnishpurgePlugin.php
+++ varnishpurge/VarnishpurgePlugin.php
@@ -5,6 +5,68 @@ namespace Craft;
 class VarnishpurgePlugin extends BasePlugin
 {
 
+    protected $_version = '0.2.0',
+      $_schemaVersion = '1.0.0',
+      $_name = 'Varnish Purge',
+      $_url = 'https://github.com/aelvan/VarnishPurge-Craft',
+      $_releaseFeedUrl = 'https://raw.githubusercontent.com/aelvan/VarnishPurge-Craft/master/releases.json',
+      $_documentationUrl = 'https://github.com/aelvan/VarnishPurge-Craft/blob/master/README.md',
+      $_description = 'Purge that Varnish cache!',
+      $_developer = 'André Elvan',
+      $_developerUrl = 'http://vaersaagod.no/',
+      $_minVersion = '2.4';
+
+    public function getName()
+    {
+        return Craft::t($this->_name);
+    }
+
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
+    public function getVersion()
+    {
+        return $this->_version;
+    }
+
+    public function getDeveloper()
+    {
+        return $this->_developer;
+    }
+
+    public function getDeveloperUrl()
+    {
+        return $this->_developerUrl;
+    }
+
+    public function getDescription()
+    {
+        return $this->_description;
+    }
+
+    public function getDocumentationUrl()
+    {
+        return $this->_documentationUrl;
+    }
+
+    public function getSchemaVersion()
+    {
+        return $this->_schemaVersion;
+    }
+
+    public function getReleaseFeedUrl()
+    {
+        return $this->_releaseFeedUrl;
+    }
+
+    public function getCraftRequiredVersion()
+    {
+        return $this->_minVersion;
+    }
+
+    
     public function init()
     {
         parent::init();
@@ -15,28 +77,7 @@ class VarnishpurgePlugin extends BasePlugin
             });
         }
     }
-
-    public function getName()
-    {
-        return Craft::t('Varnish Purge');
-    }
-
-    public function getVersion()
-    {
-        return '0.1';
-    }
-
-    public function getDeveloper()
-    {
-        return 'André Elvan';
-    }
-
-    public function getDeveloperUrl()
-    {
-        return 'http://vaersaagod.no';
-    }
-
-
+    
     public function addEntryActions()
     {
         $actions = array();
